27b991746f2076684b395a4fdb956c901ed39a2c
Gleb Musin <glebmusin77@gmail.com>
2023-02-15 01:57:40 +0500

    Updated start handler, implemented web apps for sign in and sign up, implemented sign up handler

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,8 @@
+import json
 from telebot import types, TeleBot
 from bot.handlers.start import StartHandler
 from bot.handlers.auth.sign_in import SignInHandler
+from bot.handlers.auth.sign_up import SignUpHandler
 from config.config import Config
 
 
@@ -20,13 +22,29 @@ def create_bot() -> TeleBot:
 
 def register_handlers(bot: TeleBot) -> None:
     command_handlers = {
-        "start": StartHandler,
+        "start": StartHandler
+    }
+
+    web_app_handlers = {
         "sign_in": SignInHandler,
+        "sign_up": SignUpHandler
     }
     
     for command, handler in command_handlers.items():
         bot.register_message_handler(handler, commands=[command], pass_bot=True)
 
+    for operation, handler in web_app_handlers.items():
+        bot.register_message_handler(
+            handler, 
+            content_types=["web_app_data"],
+            func=_web_app_operation_filter(operation),
+            pass_bot=True
+        )
+
 
 def run(bot: TeleBot) -> None:
     bot.infinity_polling()
+
+
+def _web_app_operation_filter(operation):
+    return lambda message: json.loads(message.web_app_data.data)["operation"] == operation
--- bot/handlers/auth/sign_in.py
+++ bot/handlers/auth/sign_in.py
@@ -1,5 +1,4 @@
-from logging import Logger
-from config.config import Config
+import json
 from telebot import types, TeleBot
 from bot.di_container.container import di_container
 from bot.usecases.auth.sign_in import SignInUsecase
@@ -8,20 +7,19 @@ from bot.usecases.auth.sign_in import SignInUsecase
 class SignInHandler:
     def __init__(self, message: types.Message, bot: TeleBot) -> None:
         self._bot = bot
-        self._logger = di_container.resolve(Logger)
         self._usecase = di_container.resolve(SignInUsecase)
         self._handle(message)
-
+    
     def _handle(self, message: types.Message) -> None:
-        markup = types.InlineKeyboardMarkup(row_width=1)
-        sign_in_page = types.WebAppInfo(Config.SIGN_IN_URL)
-        button = types.InlineKeyboardButton(text="Sign In", web_app=sign_in_page)
-        markup.add(button)
+        # data = json.loads(message.web_app_data.data)
 
-        self._bot.send_message(message.chat.id, "Please sign in for use this bot", reply_markup=markup)
+        # if data["operation"] != "sign_in":
+        #     return
 
+        self._bot.send_message(message.chat.id, str(123))
         result = self._usecase(None)
+        markup = types.ReplyKeyboardRemove()
         if result.success:
-            self._bot.send_message(message.chat.id, "Sign in is successful!")
+            self._bot.send_message(message.chat.id, "Sign in is successful!", reply_markup=markup)
         else:
             self._bot.send_message(message.chat.id, f"Sign in is failed: {result.data}")
