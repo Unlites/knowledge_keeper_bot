387d8dbce40e3b7ae1b5ffc8c2a03d131909d913
Gleb Musin <glebmusin77@gmail.com>
2023-02-16 02:02:17 +0500

    Splited userDTO and implemented pydantic validation to userSignIn DTO

---
+++ bot/handlers/message_converting.py
@@ -0,0 +1,6 @@
+def validation_errors(errors: list[dict]) -> str:
+    converted_errors_string = ""
+    for error in errors:
+        for i in range(len(error['loc'])):
+            converted_errors_string += f"\n{error['loc'][i]} - {error['msg']}"
+    return converted_errors_string
